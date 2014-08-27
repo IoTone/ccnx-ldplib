9955537e90c6571808844fd44184954bb95b64b9
David J. Kordsmeier <dkords@gmail.com>
2014-08-26 22:50:31 -0700

    refs #4 Add the library stub files

---
+++ ldputil.c
@@ -0,0 +1,49 @@
+/**
+#
+#Copyright (c) 2014 IoTone, Inc. All rights reserved.
+#
+#Redistribution and use in source and binary forms, with or without modification,
+#are permitted provided that the following conditions are met:
+#
+#    * Redistributions of source code must retain the above copyright notice,
+#      this list of conditions and the following disclaimer.
+#
+#    * Redistributions in binary form must reproduce the above copyright notice,
+#      this list of conditions and the following disclaimer in the documentation
+#      and/or other materials provided with the distribution.
+#
+#    * Neither the name of IoTone, Inc., nor the names of its
+#      contributors may be used to endorse or promote products derived from this
+#      software without specific prior written permission.
+#
+#THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+#ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+#WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+#DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+#ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+#(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+#LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+#ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+#SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**/
+
+
+
+#include "ldp.h"
+
+/* XXX This does nothing.  Get rid of it if we don't intend to build a real utlity 
+
+	Some ideas, add a command line tool that assists in testing ldp client peers to generate messages
+*/
+int main(int argc, char **argv)
+{
+	/* LDPLOG = &ldp_log_console;
+  	LDPLOG_setmask = &ldp_log_console_setlogmask;
+  	LDPLOG_setmask(LOG_MASK(LOG_INFO));
+  	openlog ("ldputil", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
+  	LDPLOG(LOG_DEBUG, "Testing our logging");
+  	closelog();
+  	*/
+	return 0;
+}
